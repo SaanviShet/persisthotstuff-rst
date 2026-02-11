eae3a3d68a4f1d9a9df86da090d0cd332b0f21fa
SaanviShet <saanvishet12@gmail.com>
2026-02-11 12:56:52 +0530

    seperated out test cases from network.rs to test folder

--- src/network.rs
+++ src/network.rs
@@ -76,7 +76,7 @@ pub struct Network {
     message_queue: VecDeque<Message>,
     
     /// Total number of replicas in the network
-    num_replicas: usize,
+    pub num_replicas: usize,
     
     /// Flag to enable/disable message delays (for testing)
     simulate_delays: bool,
@@ -203,52 +203,5 @@ impl Network {
         println!("  QC Broadcasts: {}", self.messages_by_type[2]);
         println!("  NewView: {}", self.messages_by_type[3]);
         println!("Pending messages: {}", self.pending_count());
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::crypto::sign;
-    
-    #[test]
-    fn test_network_creation() {
-        let network = Network::new(4);
-        assert_eq!(network.num_replicas, 4);
-        assert!(!network.has_messages());
-    }
-    
-    #[test]
-    fn test_send_receive() {
-        let mut network = Network::new(4);
-        let vote = Vote {
-            block_hash: 1,
-            view: 1,
-            signature: sign(0),
-        };
-        
-        network.send_vote(0, 1, vote.clone());
-        assert!(network.has_messages());
-        assert_eq!(network.pending_count(), 1);
-        
-        let msg = network.receive();
-        assert!(msg.is_some());
-        assert!(!network.has_messages());
-    }
-    
-    #[test]
-    fn test_broadcast_proposal() {
-        let mut network = Network::new(4);
-        let block = Block {
-            hash: 1,
-            parent: Some(0),
-            view: 1,
-            proposer: 0,
-            qc: None,
-        };
-        
-        network.broadcast_proposal(0, block);
-        // Should send to 3 other replicas (not to self)
-        assert_eq!(network.pending_count(), 3);
     }
 }
---
+++ tests/network_test.rs
@@ -0,0 +1,47 @@
+use persisthotstuff_rst::types::*;
+use persisthotstuff_rst::config::ReplicaId;
+use persisthotstuff_rst::crypto::sign;
+use persisthotstuff_rst::network::{Network, Message};
+
+
+#[test]
+fn test_network_creation() {
+    let network = Network::new(4);
+    assert_eq!(network.num_replicas, 4);
+    assert!(!network.has_messages());
+}
+
+#[test]
+fn test_send_receive() {
+    let mut network = Network::new(4);
+    let vote = Vote {
+        block_hash: 1,
+        view: 1,
+        signature: sign(0),
+    };
+    
+    network.send_vote(0, 1, vote.clone());
+    assert!(network.has_messages());
+    assert_eq!(network.pending_count(), 1);
+    
+    let msg = network.receive();
+    assert!(msg.is_some());
+    assert!(!network.has_messages());
+}
+
+#[test]
+fn test_broadcast_proposal() {
+    let mut network = Network::new(4);
+    let block = Block {
+        hash: 1,
+        parent: Some(0),
+        view: 1,
+        proposer: 0,
+        qc: None,
+    };
+    
+    network.broadcast_proposal(0, block);
+    // Should send to 3 other replicas (not to self)
+    assert_eq!(network.pending_count(), 3);
+}
+
